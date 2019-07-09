dae2815314ccd5887d7e4f78828145ed0c01c471
igefimov <efimov.ig@gmail.com>
2019-07-10 01:35:19 +0200

    Add more variables and example file cause variables.tfvars is in .gitignore

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,19 +1,22 @@
 terraform {
-  #  required_version = "0.11.7"
-  #  required_version = "0.11.13"
+  #  required_version = "0.11.7"  #  required_version = "0.11.13"
 }
+
 provider "google" {
   version = "2.0.0"
   project = "${var.project}"
-  region = "${var.region}"
+  region  = "${var.region}"
 }
 
 resource "google_compute_instance" "app" {
-  name = "reddit-app"
+  name         = "reddit-app"
   machine_type = "g1-small"
-  zone = "europe-west1-b"
+  zone         = "${var.zone}"
+
   tags = [
-    "reddit-app"]
+    "reddit-app",
+  ]
+
   boot_disk {
     initialize_params {
       image = "${var.disk_image}"
@@ -21,23 +24,24 @@ resource "google_compute_instance" "app" {
   }
 
   network_interface {
-    network = "default"
-    access_config {}
+    network       = "default"
+    access_config = {}
   }
 
   metadata {
     ssh-keys = "gcp:${file(var.public_key_path)}"
   }
+
   # Here we define Provisioners and how they connect to the VM(protocol, credentials)
   connection {
-    type = "ssh"
-    user = "gcp"
-    agent = false
-    private_key = "${file("/Users/efimovi/Otus/DevOps_course/.ssh/gcp")}"
+    type        = "ssh"
+    user        = "gcp"
+    agent       = false
+    private_key = "${file(var.private_key_path)}"
   }
 
   provisioner "file" {
-    source = "files/puma.service"
+    source      = "files/puma.service"
     destination = "/tmp/puma.service"
   }
 
@@ -47,15 +51,22 @@ resource "google_compute_instance" "app" {
 }
 
 resource "google_compute_firewall" "firewall_puma" {
-  name = "allow-puma-default"
+  name    = "allow-puma-default"
   network = "default"
+
   allow {
     protocol = "tcp"
+
     ports = [
-      "9292"]
+      "9292",
+    ]
   }
+
   source_ranges = [
-    "0.0.0.0/0"]
+    "0.0.0.0/0",
+  ]
+
   target_tags = [
-    "reddit-app"]
+    "reddit-app",
+  ]
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,16 +1,28 @@
 variable project {
   description = "Project ID"
 }
+
 variable region {
   description = "Region"
-  # Значение по умолчанию
+
+  # Default value
   default = "europe-west1"
 }
+
 variable public_key_path {
-  # Описание переменной
+  # Variable description
   description = "Path to the public key used for ssh access"
 }
+
 variable disk_image {
   description = "Disk image"
 }
 
+variable private_key_path {
+  description = "Path to the private key used for connection between terraform and GCP"
+}
+
+variable zone {
+  description = "Zone where instance will be created"
+  default     = "europe-west1-b"
+}
