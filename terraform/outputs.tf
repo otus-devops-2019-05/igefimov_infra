aa1e2fa34e8135e82e6f9e0ae5a68c64edd17e27
igefimov <efimov.ig@gmail.com>
2019-07-12 16:39:00 +0200

    Commenting out loadbalancer IP

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,6 +2,6 @@ output "app_external_ip" {
   value = "${google_compute_instance.app.*.network_interface.0.access_config.0.nat_ip}"
 }
 
-output "app_lb_external_ip" {
-  value = "${google_compute_global_address.lb-global-ip.address}"
-}
+//output "app_lb_external_ip" {
+//  value = "${google_compute_global_address.lb-global-ip.address}"
+//}
