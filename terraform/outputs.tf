6f61b83aed4c0e8d7ee3724784ff45d600a55dca
igefimov <efimov.ig@gmail.com>
2019-07-13 14:41:46 +0200

    Cleanup after moving main.tf to the prod and stage modules

--- terraform/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "app_external_ip" {
-  value = "${module.app.app_external_ip}"
-}
-
-//output "app_lb_external_ip" {
-//  value = "${google_compute_global_address.lb-global-ip.address}"
-//}
-
