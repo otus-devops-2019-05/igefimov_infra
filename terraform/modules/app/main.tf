22f999abcc2346a518074c4aa48ebd544541948f
igefimov <efimov.ig@gmail.com>
2019-07-21 12:45:43 +0200

    Nginx community role added and congfigured. You can access app on port 80 now

--- terraform/modules/app/main.tf
+++ terraform/modules/app/main.tf
@@ -80,3 +80,24 @@ resource "google_compute_firewall" "firewall_puma" {
     "reddit-app",
   ]
 }
+
+resource "google_compute_firewall" "firewall_nginx" {
+  name    = "allow-nginx-default"
+  network = "default"
+
+  allow {
+    protocol = "tcp"
+
+    ports = [
+      "80",
+    ]
+  }
+
+  source_ranges = [
+    "0.0.0.0/0",
+  ]
+
+  target_tags = [
+    "reddit-app",
+  ]
+}
